a29cbb0f87e6a41062b546a2dc01ded1fbbf7209
ParkMinjeong <minwjd1028@g.skku.edu>
2022-12-23 09:19:38 +0900

    221223 visualize R

--- 2 visualize/visualization.R
+++ 2 visualize/visualization.R
@@ -1,6 +1,26 @@
-personal <- read.csv("./건강검진/건강검진.csv", header=T, fileEncoding="euc-kr")
-names(personal)
+# install.packages("CGPfunctions")
+# install.packages("ggcorrplot")
 
-str(personal)
+library(ggplot2)
+library(CGPfunctions)
+library(ggcorrplot)
 
+region <- read.csv("./data/region.csv", header=T)
+brfss <- read.csv("./data/brfss.csv", header=T)
+checkup <- read.csv("./data/checkup.csv", header=T)
+
+names(brfss)
+
+PlotXTabs2(brfss, x=SEX, y=HEARTDISEASE, 
+           results.subtitle=F,
+           labels.legend=c("n", "y"))
+
+brfss$DEAF <- factor(brfss$DEAF)
+brfss$HEARTDISEASE <- factor(brfss$HEARTDISEASE)
+
+ggplot(brfss, aes(x=DEAF, fill=HEARTDISEASE)) + 
+  geom_bar(position="fill") +
+  scale_x_discrete(labels=c("0"="no", "1"="yes"))
+
+hist(brfss$HEIGHT)
 
